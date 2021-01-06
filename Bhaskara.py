36e3a972d500569458106f4d003b9a26438c7ec5
Ryan Souza <76923948+Ryrden@users.noreply.github.com>
2021-01-06 20:40:47 -0300

    problema na resolução
    adicionado uma função f(x)

--- Bhaskara.py
+++ Bhaskara.py
@@ -1,7 +1,9 @@
 import math
 
+
 def delta(a, b, c):
     return (b**2) - (4*a*c)
+
 
 def main():
     a_digitado = float(input("dado ax²+ bx + c = 0 Defina o valor de a: "))
@@ -9,16 +11,18 @@ def main():
     c_digitado = float(input("dado ax²+ bx + c = 0 Defina o valor de c: "))
     imprime_raizes(a_digitado, b_digitado, c_digitado)
 
+
 def imprime_raizes(a, b, c):
-    if (a != 0 and b != 0 and c != 0): 
+    if (a != 0 and b != 0 and c != 0):
 
         d = delta(a, b, c)
-        
+        raiz = math.sqrt(d)
         if (d > 0):
-            x1 = -b + math.sqrt(d)/2*a 
-            x2 = -b - math.sqrt(d)/2*a
-            print ("você tem duas raizes distintas")
+            x1 = (-(b) + raiz)/(2*a)
+            x2 = (-(b) - raiz)/(2*a)
+            print("você tem duas raizes distintas")
             print("suas duas raizes são x1 =", x1, "e  x2 =", x2)
+
 
         elif (d == 0):
             print("você tem uma raiz dupla")
@@ -30,5 +34,18 @@ def imprime_raizes(a, b, c):
 
     else:
         print("0 não é um número valido para uma equação do 2º grau")
+
+
+def calculaX():
+    a = float(input("dado f(x) = ax²+ bx + c Defina o valor de a: "))
+    b = float(input("dado f(x) = ax²+ bx + c Defina o valor de b: "))
+    c = float(input("dado f(x) = ax²+ bx + c Defina o valor de c: "))
+    x = int(input("Qual o valor de x você quer encontrar: "))
+
+    solve = (a * (x**2)) + (b * x) + c
+
+    print(solve)
+
+    return None
 
 main()
