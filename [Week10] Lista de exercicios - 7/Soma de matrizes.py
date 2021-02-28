2d81f7a250633119f23276c18ad0bcfe6961e38a
Ryan Souza <76923948+Ryrden@users.noreply.github.com>
2021-02-27 21:57:52 -0300

    Atividades

--- [Week10] Lista de exercicios - 7/Soma de matrizes.py
+++ [Week10] Lista de exercicios - 7/Soma de matrizes.py
@@ -1,16 +1,16 @@
 def soma_matrizes(m1, m2):
 
-    if len(m1) == len(m1):
+    if len(m1) == len(m1): #comparando Linhas
 
-        if len(m1[0]) == len(m2[0]):
+        if len(m1[0]) == len(m2[0]): #comparando Colunas
             lista_final = []
-            lista_transporte = []
+            lista_transporte = [] 
 
-            for i in range(len(m1)):
+            for i in range(len(m1)): 
 
                 for j in range(0, (len(m1[i]))):
-                    aux = 0
-                    aux = m1[i][j] + m2[i][j]
+                    aux = 0                      #variável auxiliar para adicionar valores para lista_transporte
+                    aux = m1[i][j] + m2[i][j] 
                     lista_transporte.append(aux)
                 lista_final.append(lista_transporte)
                 lista_transporte = []
@@ -18,12 +18,6 @@ def soma_matrizes(m1, m2):
             return lista_final
 
         else:
-            return False
+            return False       #Caso Coluna diferente 
     else:
-        return False
-
-
-m1 = [[1, 2, 3], [4, 5, 6]]
-m2 = [[2, 3, 4], [5, 6, 7]]
-
-soma_matrizes(m1, m2)
+        return False           #Caso linha diferente
