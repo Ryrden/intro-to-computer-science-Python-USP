0c4da8690b0dfe442e2dfa8be969f525ae315fd7
Ryan Souza <76923948+Ryrden@users.noreply.github.com>
2021-02-28 01:17:16 -0300

    atividades

--- [Week10] Lista de exercicios - 7/Soma de matrizes.py
+++ [Week10] Lista de exercicios - 7/Soma de matrizes.py
@@ -8,7 +8,7 @@ def soma_matrizes(m1, m2):
 
             for i in range(len(m1)): #percorrendo linha
 
-                for j in range(0, (len(m1[i]))): #percorrendo coluna 
+                for j in range(0, len(m1[i])): #percorrendo coluna 
                     aux = 0                      #variável auxiliar para adicionar valores para lista_transporte
                     aux = m1[i][j] + m2[i][j] 
                     lista_transporte.append(aux)
