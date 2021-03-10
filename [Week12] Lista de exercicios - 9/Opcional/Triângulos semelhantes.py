0ca53576034d3fe807c3c7c04040e0cf27318ec3
Ryan Souza <76923948+Ryrden@users.noreply.github.com>
2021-03-10 14:53:49 -0300

    atividades

---
+++ [Week12] Lista de exercicios - 9/Opcional/Triângulos semelhantes.py
@@ -0,0 +1,38 @@
+class Triangulo:
+
+    def __init__(self, lado1, lado2, lado3):
+
+        self.a = lado1
+        self.b = lado2
+        self.c = lado3
+
+    def perimetro(self):
+        return self.a + self.b + self.c
+
+    def tipo_lado(self):
+        
+        lado1, lado2, lado3 = self.a, self.b, self.c
+
+        if lado1 == lado2 == lado3:
+            return 'equilátero'
+        
+        if lado1 == lado2 or lado2 == lado3 or lado1 == lado3:
+            return 'isóceles'
+        
+        if not lado1 == lado2 == lado3:
+            return 'escaleno'
+
+    def retangulo(self):
+
+        a, b, c = self.a, self.b, self.c
+
+        #verificando se é retângulo através do teorema de pitagóras
+        if a**2 + b**2 == c**2:
+            return True
+        else:
+            return False
+
+    def semelhantes(self, triangulo):
+        
+        
+        return None
