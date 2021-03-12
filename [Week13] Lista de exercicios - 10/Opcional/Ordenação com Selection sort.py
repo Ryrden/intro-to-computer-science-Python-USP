034eadd68d44f1e1b6281778ace83ce30cca0c0e
Ryan Souza <76923948+Ryrden@users.noreply.github.com>
2021-03-12 00:23:04 -0300

    atividade - Ordenação via algoritmo
    Seleção Direta

---
+++ [Week13] Lista de exercicios - 10/Opcional/Ordenação com Selection sort.py
@@ -0,0 +1,27 @@
+def ordena(lista):
+    
+    Endlist = len(lista)
+
+    for i in range(Endlist-1):
+
+        Menor_posição = i
+
+        for j in range(i+1, Endlist):
+            if lista[j] < lista[Menor_posição]:
+                Menor_posição = j
+
+        lista[i], lista[Menor_posição] = lista[Menor_posição], lista[i]
+
+    return lista
+
+# Verificação
+if __name__ == "__main__":
+    
+    import Listas_grandes
+
+    lista = Listas_grandes.lista_grande(10)
+
+    print(
+        f"Antes de ser Ordenada \t-> {lista}\n\n"
+        f"Depois de ser Ordenada \t-> {ordena(lista)}"
+        )
